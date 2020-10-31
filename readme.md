885442d00309fec335123a358dea6689a337ad69
Finn Pauls <mbp@pauls.dev>
2020-10-31 16:41:23 +0000

    First working version

---
+++ readme.md
@@ -0,0 +1,4 @@
+[ ] nice fonts reset
+[ ] eslint
+[ ] github publishing
+[ ] dev and production typescript options -> https://devblogs.microsoft.com/typescript/announcing-typescript-4-1-beta/#jsx-factories
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function App() {
+    return <Main>Hello</Main>
+}
+
+const Main = styled.div`
+    font-size: 100px;
+`
