1fe7d479e7cdda77b785bd1df1d712409eabdedf
Finn Pauls <mbp@pauls.dev>
2020-10-31 17:52:57 +0000

    Add linting

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 
 export default function App() {
-    return <Main>a nice new project</Main>
+  return <Main>a nice new project</Main>
 }
 
 const Main = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
 
-    background-color: #EEEEE4;
-    color: #242422;
-    font-size: 75px;
+  background-color: #eeeee4;
+  color: #242422;
+  font-size: 75px;
 `
