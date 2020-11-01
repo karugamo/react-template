24f267c35adf433ae31fe5b19f9c3c2f13394ff4
Finn Pauls <mbp@pauls.dev>
2020-11-01 18:07:33 +0000

    Add husky

--- index.tsx
+++ index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 
 import App from './src/App'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
-)
+ReactDOM.render(<App />, document.getElementById('root'))
+
+errror
